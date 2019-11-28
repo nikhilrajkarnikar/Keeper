976e807a5619fd92ae4c290e5cdee2222bd2a760
nikhilrajkarnikar <52517903+nikhilrajkarnikar@users.noreply.github.com>
2019-11-28 18:42:14 +0545

    Renamed the build folder to docs and uploaded to git

---
+++ docs/precache-manifest.df122366b12266d18fef209ad3341bdb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf2a8002296e79b80630574075bbfdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b8d33756ef080e6e7a5",
+    "url": "/static/js/2.c6579aae.chunk.js"
+  },
+  {
+    "revision": "83c298eb998655375f4f",
+    "url": "/static/js/main.480fefed.chunk.js"
+  },
+  {
+    "revision": "1306a539dc51cae34ec3",
+    "url": "/static/js/runtime-main.9688630e.js"
+  }
+]);
